ed1aca6092c6c3f98f98610f47e2da8cf93e46cf
David Silva Telles <davidsilvatelles2023@gmail.com>
2026-05-25 10:55:19 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ mongoose.connection.on('connected', () => {
     console.log('Connect to MongoDB');
 });
 
+const musicasRouter = require('./routes/musicas');
+const filmesRouter = require('./routes/filmes');
+const seriesRouter = require('./routes/series');
+const jogosRouter = require('./routes/jogos');
+const livrosRouter = require('./routes/livros');
+
+app.use('/musicas', musicasRouter);
+app.use('/filmes', filmesRouter);
+app.use('/series', seriesRouter);
+app.use('/jogos', jogosRouter);
+app.use('/livros', livrosRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
